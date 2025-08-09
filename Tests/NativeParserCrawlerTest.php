2190af9c5cd57fb68658c2b80c7de4d982eb7bab
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-08-10 00:28:14 +0200

    chore: heredoc indentation as of PHP 7.3
    https://www.php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc

--- Tests/NativeParserCrawlerTest.php
+++ Tests/NativeParserCrawlerTest.php
@@ -24,16 +24,16 @@ class NativeParserCrawlerTest extends AbstractCrawlerTestCase
 
         $crawler = $this->createCrawler();
         $crawler->addHtmlContent(<<<'EOF'
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-    <head>
-    </head>
-    <body>
-        <div><a href="#"></div>
-    </body>
-    </body>
-</html>
-EOF
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+            <html>
+                <head>
+                </head>
+                <body>
+                    <div><a href="#"></div>
+                </body>
+                </body>
+            </html>
+            EOF
             , 'UTF-8');
 
         $errors = libxml_get_errors();
@@ -50,15 +50,15 @@ EOF
 
         $crawler = $this->createCrawler();
         $crawler->addXmlContent(<<<'EOF'
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-    <head>
-    </head>
-    <body>
-        <nav><a href="#"><a href="#"></nav>
-    </body>
-</html>
-EOF
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+            <html>
+                <head>
+                </head>
+                <body>
+                    <nav><a href="#"><a href="#"></nav>
+                </body>
+            </html>
+            EOF
             , 'UTF-8');
 
         $this->assertGreaterThan(1, libxml_get_errors());
